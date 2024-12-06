039d22fdff1beba5e8e469d0372f1c0d65cdd4b9
Vagucor <vagucor518@g.educaand.es>
2024-12-06 20:18:24 +0100

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,11 @@
+package pruebas;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        // Imprimir mensaje en la consola
+        System.out.println("Hola GiT!!");
+    }
+
+}
